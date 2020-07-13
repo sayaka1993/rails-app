3e42a91139d992005143f4b50272468a43e03eb8
sayakairie <kuidaore.like.saya@gmail.com>
2020-07-13 12:51:56 +0900

    Commit-second

---
+++ app/models/post.rb
@@ -0,0 +1,6 @@
+class Post < ApplicationRecord
+    validates :content, {presence: true, length: {maximum: 140}}
+    # user_idカラムにバリデーションを追加してください
+    validates :user_id, {presence: true}
+    
+end
